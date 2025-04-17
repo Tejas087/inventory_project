e1f93e45c4508e6f356a7bff2567d5939e1c71d5
Tejas087 <115393712+Tejas087@users.noreply.github.com>
2025-04-17 15:49:37 +0530

    commit

--- inventory/products/tests.py
+++ inventory/products/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from .models import Product
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class ProductAPITest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.admin = User.objects.create_superuser('admin', 'admin@test.com', 'adminpass')
+        self.product = Product.objects.create(
+            name="Test Product",
+            description="Test Desc",
+            price=100.0,
+            stock=5
+        )
+
+    def test_product_list(self):
+        response = self.client.get('/api/products/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_product_create_admin_only(self):
+        self.client.login(username='admin', password='adminpass')
+        response = self.client.post('/api/products/', {
+            'name': 'New Item',
+            'description': 'New Desc',
+            'price': 50,
+            'stock': 20
+        })
+        self.assertEqual(response.status_code, 201)
+
+    def test_unauth_create_denied(self):
+        response = self.client.post('/api/products/', {
+            'name': 'Blocked Item',
+            'description': 'Desc',
+            'price': 20,
+            'stock': 10
+        })
+        self.assertEqual(response.status_code, 403)
--- inventory/products/views.py
+++ inventory/products/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters, permissions
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user and request.user.is_staff
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAdminOrReadOnly]  
+
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        low_stock = self.request.query_params.get('low_stock')
+        if low_stock == 'true':
+            queryset = queryset.filter(stock__lt=10)
+        return queryset
